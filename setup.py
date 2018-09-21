60c18d74ca5a29fce04365d37c1f3e03fa6ac855
lambtron <andyjiang@gmail.com>
2018-09-21 14:10:07 -0700

    bump minor version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-clubspeed',
-      version='0.2.0',
+      version='0.2.1',
       description='Singer.io tap for extracting data from the Clubspeed API',
       author='lambtron',
       author_email="andyjiang@gmail.com",
