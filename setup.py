f859ec4894f20a52f922e3b9f8808ebfa4ebce74
Kyle Allan <kyle@stitchdata.com>
2018-12-11 18:21:32 +0000

    bump to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-clubspeed',
-      version='0.3.6',
+      version='1.0.0',
       description='Singer.io tap for extracting data from the Clubspeed API',
       author='lambtron',
       author_email="andyjiang@gmail.com",
